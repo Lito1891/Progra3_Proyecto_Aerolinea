3ad3a6767adfb1e4f961fe26c90f134d1dc0ab4c
JorgeSolano28 <jorge.solanobarboza28@gmail.com>
2026-08-09 13:16:04 -0600

    Se añade controlador y modelo para inicio de sesión en el login.html, además se modifica el login y queda pendiente crear el dashboard

---
+++ Progra3-Proyecto-Aerolinea/Models/modeloLogin.js
@@ -0,0 +1,7 @@
+//datos modelos para usuario y contraseña en simulación para el inicio de sesión, estos datos serán usados por el controlador
+
+//array para validar el nombre de usuario, de momento para prueba solamente habrá uno
+let usuarioLogin =["admin"]
+
+//array para validar la contraseña del usuario, de momento para prueba solamente habrá uno
+let contraLogin =["admin"]
