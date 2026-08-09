e4d86418399837fa6ab7ca967b74acbc7d59e2b1
cris-aerolina <cmadrigal1012@gmail.com>
2026-08-09 17:36:19 -0600

    Se agregan comentarios para cada parte del código, se agrega css para mantener los acordeones ocultos y se agrega su respectivo js, se ajusta para que todos los acordeones redireccionen a la pagina de pago

--- Progra3-Proyecto-Aerolinea/Assets/js/vuelos.js
+++ Progra3-Proyecto-Aerolinea/Assets/js/vuelos.js
@@ -17,3 +17,63 @@
         }, false)
     })
 })()
+
+
+//para evitar fechas anteriores y que la fecha seleccionada como de regreso tenga restriccion de que tiene que ser mayar a un dia de diferencia
+const hoy = new Date().toISOString().split("T")[0];
+
+const fechaIda = document.getElementById("fecha");
+const fechaRegreso = document.getElementById("fechaRegreso");
+
+// No permitir fechas anteriores a hoy
+fechaIda.min = hoy;
+fechaRegreso.min = hoy;
+
+// La fecha de regreso no puede ser anterior a la fecha de ida
+fechaIda.addEventListener("change", function () {
+    fechaRegreso.min = fechaIda.value;
+});
+
+
+//para esconder los paises
+document.getElementById("formularioVuelos").addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    // Obtener la ciudad seleccionada
+    const destino = document.getElementById("destino").value;
+
+    // Ocultar todos los resultados
+    const resultados = document.querySelectorAll(".resultado-vuelo");
+
+    resultados.forEach(function(resultado) {
+        resultado.classList.remove("activo");
+    });
+
+    // Relacionar cada ciudad con su sección
+    const secciones = {
+        paris: "resultadoFrancia",
+        tokio: "resultadoTokio",
+        rio: "resultadoRio",
+        sanJose: "resultadoSanJose",
+        cusco: "resultadoCusco",
+        groenlandia: "resultadoGroenlandia"
+    };
+
+    // Obtener la sección correspondiente
+    const seccion = document.getElementById(secciones[destino]);
+
+    // Mostrarla
+    if (seccion) {
+        seccion.classList.add("activo");
+
+        // Desplazarse hasta ella
+        seccion.scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    }
+
+});
+
+
