7b1cf7f65ba9eb612254af5db9f2f5b5855b341d
JorgeSolano28 <jorge.solanobarboza28@gmail.com>
2026-08-13 18:45:36 -0600

    Mover carpeta Assets dentro de Views y actualizar rutas

--- Progra3-Proyecto-Aerolinea/Models/modeloDestinos.js
+++ Progra3-Proyecto-Aerolinea/Models/modeloDestinos.js
@@ -8,7 +8,7 @@ const listaDestinos = [
     descripcion: "Camina por los Campos Elíseos, admira la Torre Eiffel iluminada y vive la experiencia europea.",
     precio: "599",
     categoria: "internacional",
-    imagen: "../Assets/img/fotosDestinos/paris.jpg",
+    imagen: "Assets/img/fotosDestinos/paris.jpg",
     },
     {
     id: 2,
@@ -16,7 +16,7 @@ const listaDestinos = [
     descripcion: "Fusión asombrosa de santuarios antiguos y rascacielos tecnológicos llenos de luces de neón.",
     precio: "899",
     categoria: "internacional",
-    imagen: "../Assets/img/fotosDestinos/tokio.jpg",
+    imagen: "Assets/img/fotosDestinos/tokio.jpg",
     },
     {
     id: 3,
@@ -24,7 +24,7 @@ const listaDestinos = [
     descripcion: "Disfruta de las costas doradas de Copacabana, explora el Cristo Redentor y vive el ritmo tropical.",
     precio: "420",
     categoria: "internacional",
-    imagen: "../Assets/img/fotosDestinos/riodejaneiro.jpg",
+    imagen: "Assets/img/fotosDestinos/riodejaneiro.jpg",
     },
     {
     id: 4,
@@ -32,7 +32,7 @@ const listaDestinos = [
     descripcion: "Descubre exuberantes bosques tropicales, volcanes imponentes y la calidez del estilo de vida Pura Vida.",
     precio: "150",
     categoria: "nacional",
-    imagen: "../Assets/img/fotosDestinos/costarica.jpg",
+    imagen: "Assets/img/fotosDestinos/costarica.jpg",
     },
     {
     id: 5,
@@ -40,7 +40,7 @@ const listaDestinos = [
     descripcion: "Explora la histórica capital del Imperio Inca y maravíllate con los misterios arqueológicos de Machu Picchu.",
     precio: "380",
     categoria: "internacional",
-    imagen: "../Assets/img/fotosDestinos/peru.jpg",
+    imagen: "Assets/img/fotosDestinos/peru.jpg",
     },
     {
     id: 6,
@@ -48,7 +48,7 @@ const listaDestinos = [
     descripcion: "Una aventura única entre icebergs gigantescos, auroras boreales espectaculares y paisajes árticos.",
     precio: "1,250",
     categoria: "internacional",
-    imagen: "../Assets/img/fotosDestinos/groenlandia.jpg",
+    imagen: "Assets/img/fotosDestinos/groenlandia.jpg",
     },
     {
     id: 7,
@@ -56,7 +56,7 @@ const listaDestinos = [
     descripcion: "Pasea junto al río Támesis, admira el emblemático Big Ben y descubre la rica historia de la capital británica.",
     precio: "650",
     categoria: "internacional",
-    imagen: "../Assets/img/fotosDestinos/londres.jpg",
+    imagen: "Assets/img/fotosDestinos/londres.jpg",
     },
     {
     id: 8,
@@ -64,7 +64,7 @@ const listaDestinos = [
     descripcion: "Explora el bullicioso entorno de Manhattan, admira el Empire State Building y vive la energía de la ciudad que nunca duerme.",
     precio: "750",
     categoria: "internacional",
-    imagen: "../Assets/img/fotosDestinos/nuevayork.jpg",
+    imagen: "Assets/img/fotosDestinos/nuevayork.jpg",
     },
     {
     id: 9,
@@ -72,7 +72,7 @@ const listaDestinos = [
     descripcion: "Recorre las calles históricas de la ciudad eterna, admira el Coliseo y disfruta de la deliciosa cocina italiana.",
     precio: "500",
     categoria: "internacional",
-    imagen: "../Assets/img/fotosDestinos/roma.jpg",
+    imagen: "Assets/img/fotosDestinos/roma.jpg",
     },
     {
     id: 10,
@@ -80,7 +80,7 @@ const listaDestinos = [
     descripcion: "Relájate en playas de arena blanca, explora arrecifes de coral y disfruta de la vibrante vida nocturna.",
     precio: "400",
     categoria: "internacional",
-    imagen: "../Assets/img/fotosDestinos/cancun.jpg",
+    imagen: "Assets/img/fotosDestinos/cancun.jpg",
     },
     {
     id: 11,
@@ -88,7 +88,7 @@ const listaDestinos = [
     descripcion: "Descubre las majestuosas pirámides de Giza, navega por el río Nilo y explora la rica historia del Antiguo Egipto.",
     precio: "600",
     categoria: "internacional",
-    imagen: "../Assets/img/fotosDestinos/elcairo.jpg",
+    imagen: "Assets/img/fotosDestinos/elcairo.jpg",
     },
     {
     id: 12,
@@ -96,7 +96,7 @@ const listaDestinos = [
     descripcion: "Explora la ciudad de Sídney con su icónico puente, disfruta de sus hermosas playas y descubre la naturaleza exuberante.",
     precio: "900",
     categoria: "internacional",
-    imagen: "../Assets/img/fotosDestinos/sidney.jpg",
+    imagen: "Assets/img/fotosDestinos/sidney.jpg",
     },
     ];
 // Función para obtener y filtrar destinos
